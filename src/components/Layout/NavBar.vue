aba946a42eb79de6c47c6a3f6605b58b83195ae3
Kevin Ruiz <kevingerardoruiz.2001@gmail.com>
2026-02-17 11:40:25 +0100

    Actualizando el json de comentarios

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref<number>(0)
+
+function increment() {
+    counter.value++
+}
+</script>
 <template>
   <h2>NavBar</h2>
+  <p>Counter: {{ counter }}</p>
 </template>
