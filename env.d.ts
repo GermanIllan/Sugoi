00081bab21c621430b0ad70438bf8ca055e5ce7a
troyanojoi-sour <troyanojoi@gmail.com>
2026-02-25 10:29:42 +0100

    update env and package

--- env.d.ts
+++ env.d.ts
@@ -6,32 +6,4 @@ declare module "*.vue" {
     export default component;
 }
 
-declare module "*.webp" {
-    const src: string;
-    export default src;
-}
 
-declare module "*.png" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.jpg" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.jpeg" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.gif" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.svg" {
-    const src: string;
-    export default src;
-}
